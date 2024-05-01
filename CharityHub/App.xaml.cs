206eace171d955b4337676f2aea801966057f4b2
Данил Ярмоленко <daniliarmolenko@gmail.com>
2024-05-01 23:28:17 +0200

    Add project files.

---
+++ CharityHub/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CharityHub
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
